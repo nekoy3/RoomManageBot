4dbfed3ec776cee02903582e4b47a52431035443
nekoy3 <nekoyama3.n@gmail.com>
2022-03-24 17:13:34 +0900

    各修正・機能変更
    config.iniが存在するが設定が間違えていた場合、不十分な状態で起動してしまう不具合を対策
    embedのfooterを削除
    異常な人数入退室の際にembedが表示されない不具合を修正
    異常な人数入退室の際に人数調整時コンソールにエラーを出力し正常に動作しない不具合を修正
    コマンド名「enter」を「in」に、「exit」を「out」に修正
    
    ログファイルを毎日再生成するように機能変更（まだ未実装）

--- room_manage.py
+++ room_manage.py
@@ -6,23 +6,26 @@ import configparser
 import glob
 import os
 import sys
+import datetime
+import asyncio
 
 bot = discord.Bot()
 config = configparser.ConfigParser()
 try:
     config.read('config.ini', encoding="utf-8_sig")
+
+    first_server_name = str(config['SERVER']['first_server_name'])
+    second_server_name = str(config['SERVER']['second_server_name'])
+
+    TOKEN = str(config['TOKEN']['token'])
+
+    type_dict = {'one': config['SERVER']['first_server_color'], 'two': config['SERVER']['second_server_color'], 'er': config['OTHER']['error_message_color']}
+    id_dict = {'one': [config['SERVER']['first_server_id'], config['CHANNEL']['first_channel_id']], 'two': [config['SERVER']['second_server_id'], config['CHANNEL']['second_channel_id']]}
+    max_count = int(config['OTHER']['max_roomcount'])
+
 except:
-    print("config.iniが存在しません")
+    print("config.iniが存在しないか、設定が間違っています。")
     exit()
-
-first_server_name = str(config['SERVER']['first_server_name'])
-second_server_name = str(config['SERVER']['second_server_name'])
-
-TOKEN = str(config['TOKEN']['token'])
-
-type_dict = {'one': config['SERVER']['first_server_color'], 'two': config['SERVER']['second_server_color'], 'er': config['OTHER']['error_message_color']}
-id_dict = {'one': [config['SERVER']['first_server_id'], config['CHANNEL']['first_channel_id']], 'two': [config['SERVER']['second_server_id'], config['CHANNEL']['second_channel_id']]}
-max_count = int(config['OTHER']['max_roomcount'])
 
 print('Starting...')
 
@@ -47,23 +50,33 @@ try:
 except:
     continue_flag = False
 
-if continue_flag:
+#最新(latest)のログファイルを読み取り、最終行から最新の部屋人数を取得
+def read_latest_log():
     try:
         list_of_files = glob.glob('./logs/*')
         fn = max(list_of_files, key = os.path.getctime)
         print("latest log -> " + fn)
         last_log = tail(fn)
-        #引数でcontinueが与えられた場合は人数を引き継ぐ
-        count = int(last_log.split('sum:')[1])
+        cnt = int(last_log.split('sum:')[1])
     except Exception as e:
         print(e)
-        count = 0
+        cnt = 0
+    return cnt
+
+if continue_flag:
+    count = read_latest_log()
 else:
     count = 0
 
 print("count = " + str(count))
-filename = time.strftime("room-%Y-%m-%d-%H-%M-%S") + ".log"
-f = open("./logs/" + filename, "a")
+
+#ログファイル作成
+def make_logfile():
+    global f
+    filename = time.strftime("room-%Y-%m-%d-%H-%M-%S") + ".log"
+    f = open("./logs/" + filename, "a")
+
+make_logfile()
 
 def count_manage(n, set_boolean):
     global count
@@ -74,13 +87,49 @@ def count_manage(n, set_boolean):
 
 def add_embed(title, descrip, type):
     embed = discord.Embed(title=title, description=descrip, color=int(type_dict[type], 16))
-    embed.set_footer(text="部屋人数管理システム")
     return embed
 
 def write_logfile(username, c, io_type, server_name):
     f.write(f"{time.strftime('%Y/%m/%d %H:%M:%S')} {server_name} {username} {io_type}:{c} sum:{c}\n")
 
 write_logfile('system', count, 'start', 'system')
+
+#毎日午前二時にログファイルを作り直す
+def remake_logfile():
+    global f
+    f.close()
+    make_logfile()
+    write_logfile('system', count, 'start', 'system')
+    print("remaked logfile")
+
+#特定の時刻にメソッドを実行する
+#参考　https://stackoverflow.com/questions/51292027/how-to-schedule-a-task-in-asyncio-so-it-runs-at-a-certain-date
+#また、毎日午前二時にログファイルを作り直すのを実行するようにした
+async def wait_until(dt):
+    now = datetime.datetime.now()
+    await asyncio.sleep((dt - now).total_seconds())
+
+async def sleep_one_day():
+    await asyncio.sleep(30) #86400
+
+async def run_at(dt, coro):
+    await wait_until(dt)
+    await coro()
+    while True:
+        sleep_one_day()
+        await coro()
+
+#時刻計算の参考 https://note.nkmk.me/python-datetime-usage/
+dt_next_day = datetime.datetime.today().day + 1
+dt_schedule = datetime.datetime(datetime.datetime.today().year, datetime.datetime.today().month, dt_next_day, 2, 0, 0)
+
+dt_schedule = datetime.datetime.today() + datetime.timedelta(seconds=30)
+async def log_schedule():
+    loop = asyncio.get_event_loop()
+    loop.create_task(await run_at(dt_schedule, remake_logfile()))
+    loop.run_forever()
+
+asyncio.ensure_future(log_schedule())
 
 #初期設定
 @bot.listen()
@@ -92,7 +141,7 @@ async def on_ready():
     for i in chs:
         await i.send("部屋人数管理システムを起動しました。現在の部屋人数は" + str(count) + "人です。異なる場合は/setコマンドを使用してください。")
 
-@bot.slash_command(guild_ids=[id_dict['one'][0], id_dict['two'][0]])
+@bot.slash_command(guild_ids = [id_dict['one'][0], id_dict['two'][0]], name = "in")
 async def enter(
     ctx,
     num: Option(int, '利用人数を入力してください'),
@@ -100,9 +149,9 @@ async def enter(
     one_ch, two_ch = bot.get_partial_messageable(id_dict['one'][1]), bot.get_partial_messageable(id_dict['two'][1])
     count_manage(num, False)
     if count > max_count:
-        count_manage(-num)
-        await ctx.respond("エラー")
+        count_manage(-num, False)
         embed = add_embed("満員", f"同時利用人数は{max_count}人以下にして下さい。", "er")
+        await ctx.respond(embed=embed)
         return
 
     if str(ctx.channel.id) == id_dict['one'][1]:
@@ -117,17 +166,17 @@ async def enter(
         await one_ch.send(embed=embed)
         write_logfile(ctx.author, num, "in", second_server_name)
 
-@bot.slash_command(guild_ids=[id_dict['one'][0], id_dict['two'][0]])
-async def exit(
+@bot.slash_command(guild_ids = [id_dict['one'][0], id_dict['two'][0]], name = "out")
+async def out(
     ctx,
     num: Option(int, '退出人数を入力してください'),
 ):
     one_ch, two_ch = bot.get_partial_messageable(id_dict['one'][1]), bot.get_partial_messageable(id_dict['two'][1])
     count_manage(-num, False)
     if count < 0:
-        count_manage(num)
-        await ctx.respond("エラー")
+        count_manage(num, False)
         embed = add_embed("エラー", "0人を下回らないで下さい。", "er")
+        await ctx.respond(embed=embed)
         return
 
     if str(ctx.channel.id) == id_dict['one'][1]:
