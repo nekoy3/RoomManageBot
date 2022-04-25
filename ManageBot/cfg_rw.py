7ecd96d970e856e7db504a9068f0a9e003f3f45c
nekoy3 <nekoyama3.n@gmail.com>
2022-04-25 17:15:05 +0900

    ファイル構成変更

---
+++ ManageBot/cfg_rw.py
@@ -0,0 +1,46 @@
+import configparser
+import os
+
+config = configparser.ConfigParser()
+
+class ConfigClass:
+    global config
+    def __init__(self):
+        self.config = config
+
+def create_config():
+    config.read('config.ini', encoding="utf-8_sig")
+
+    config['SERVER'] = {'first_server_name': '', 'second_server_name': '', 'first_server_color': '0x66A6FF', 'second_server_color': '0xCA80FF', 'first_server_id': '', 'second_server_id': ''}
+    config['CHANNEL'] = {'first_channel_id': '', 'second_channel_id': ''}
+    config['TOKEN'] = {'token': ''}
+    config['OTHER'] = {'max_roomcount': '10', 'error_message_color': '0xFF0000'}
+
+    with open('config.ini', 'w') as configfile:
+        config.write(configfile)
+
+def main():
+    global config
+    try:
+        config.read('config.ini', encoding="utf-8_sig")
+
+        configClass = ConfigClass()
+        configClass.first_server_name = str(config['SERVER']['first_server_name'])
+        configClass.second_server_name = str(config['SERVER']['second_server_name'])
+
+        configClass.TOKEN = str(config['TOKEN']['token'])
+
+        configClass.type_dict = {'one': config['SERVER']['first_server_color'], 'two': config['SERVER']['second_server_color'], 'er': config['OTHER']['error_message_color']}
+        configClass.id_dict = {'one': [config['SERVER']['first_server_id'], config['CHANNEL']['first_channel_id']], 'two': [config['SERVER']['second_server_id'], config['CHANNEL']['second_channel_id']]}
+        configClass.max_count = int(config['OTHER']['max_roomcount'])
+    except:
+        print("config.iniが存在しないか、設定が間違っています。")
+        #ファイルの存在確認(カレントディレクトリにconfig.iniがあるか)
+        if not os.path.isfile('config.ini'):
+            create_config()
+        exit()
+    else:
+        return configClass
+
+if __name__ == '__main__':
+    main()
--- ManageBot/room_manage.py
+++ ManageBot/room_manage.py
@@ -2,30 +2,17 @@
 import discord
 import time
 from discord.commands import Option
-import configparser
 import glob
 import os
 import sys
 from discord.ext import tasks
 from datetime import datetime 
 
+import cfg_rw
+
 bot = discord.Bot()
-config = configparser.ConfigParser()
-try:
-    config.read('config.ini', encoding="utf-8_sig")
 
-    first_server_name = str(config['SERVER']['first_server_name'])
-    second_server_name = str(config['SERVER']['second_server_name'])
-
-    TOKEN = str(config['TOKEN']['token'])
-
-    type_dict = {'one': config['SERVER']['first_server_color'], 'two': config['SERVER']['second_server_color'], 'er': config['OTHER']['error_message_color']}
-    id_dict = {'one': [config['SERVER']['first_server_id'], config['CHANNEL']['first_channel_id']], 'two': [config['SERVER']['second_server_id'], config['CHANNEL']['second_channel_id']]}
-    max_count = int(config['OTHER']['max_roomcount'])
-
-except:
-    print("config.iniが存在しないか、設定が間違っています。")
-    exit()
+cfg = cfg_rw.main()
 
 print('Starting...')
 
@@ -86,7 +73,7 @@ def count_manage(n, set_boolean):
         count += n
 
 def add_embed(title, descrip, type):
-    embed = discord.Embed(title=title, description=descrip, color=int(type_dict[type], 16))
+    embed = discord.Embed(title=title, description=descrip, color=int(cfg.type_dict[type], 16))
     return embed
 
 def write_logfile(username, c, io_type, server_name, sum):
@@ -140,41 +127,41 @@ async def on_ready():
     print('Logged in as\n' + bot.user.name + "\n" + str(bot.user.id) + "\n------")
     await bot.change_presence(status=discord.Status.online, activity=discord.Game('/in, /out'))
     await bot.user.edit(username='部屋人数管理システム')
-    chs = [bot.get_partial_messageable(id_dict['one'][1]), bot.get_partial_messageable(id_dict['two'][1])]
+    chs = [bot.get_partial_messageable(cfg.id_dict['one'][1]), bot.get_partial_messageable(cfg.id_dict['two'][1])]
     for i in chs:
         await i.send("部屋人数管理システムを起動しました。現在の部屋人数は" + str(count) + "人です。異なる場合は/setコマンドを使用してください。")
 
-@bot.slash_command(guild_ids = [id_dict['one'][0], id_dict['two'][0]], name = "in", description="部屋に入室するときのコマンドです。")
+@bot.slash_command(guild_ids = [cfg.id_dict['one'][0], cfg.id_dict['two'][0]], name = "in", description="部屋に入室するときのコマンドです。")
 async def enter(
     ctx,
     num: Option(int, '利用人数を入力してください'),
 ):
-    one_ch, two_ch = bot.get_partial_messageable(id_dict['one'][1]), bot.get_partial_messageable(id_dict['two'][1])
+    one_ch, two_ch = bot.get_partial_messageable(cfg.id_dict['one'][1]), bot.get_partial_messageable(cfg.id_dict['two'][1])
     count_manage(num, False)
-    if count > max_count:
+    if count > cfg.max_count:
         count_manage(-num, False)
-        embed = add_embed("満員", f"同時利用人数は{max_count}人以下にして下さい。", "er")
+        embed = add_embed("満員", f"同時利用人数は{cfg.max_count}人以下にして下さい。", "er")
         await ctx.respond(embed=embed)
         return
 
-    if str(ctx.channel.id) == id_dict['one'][1]:
-        embed = add_embed("利用通知", f'{first_server_name}で{num}人入室しました。現在の利用人数は{count}人です。', "one")
+    if str(ctx.channel.id) == cfg.id_dict['one'][1]:
+        embed = add_embed("利用通知", f'{cfg.first_server_name}で{num}人入室しました。現在の利用人数は{count}人です。', "one")
         await ctx.respond(embed=embed)
         await two_ch.send(embed=embed)
-        write_logfile(ctx.author, num, "in", first_server_name, count)
+        write_logfile(ctx.author, num, "in", cfg.first_server_name, count)
 
-    elif str(ctx.channel.id) == id_dict['two'][1]:
-        embed = add_embed("利用通知", f'{second_server_name}で{num}人入室しました。現在の利用人数は{count}人です。', "two")
+    elif str(ctx.channel.id) == cfg.id_dict['two'][1]:
+        embed = add_embed("利用通知", f'{cfg.second_server_name}で{num}人入室しました。現在の利用人数は{count}人です。', "two")
         await ctx.respond(embed=embed)
         await one_ch.send(embed=embed)
-        write_logfile(ctx.author, num, "in", second_server_name, count)
+        write_logfile(ctx.author, num, "in", cfg.second_server_name, count)
 
-@bot.slash_command(guild_ids = [id_dict['one'][0], id_dict['two'][0]], name = "out", description="部屋を退室するときのコマンドです。")
+@bot.slash_command(guild_ids = [cfg.id_dict['one'][0], cfg.id_dict['two'][0]], name = "out", description="部屋を退室するときのコマンドです。")
 async def out(
     ctx,
     num: Option(int, '退出人数を入力してください'),
 ):
-    one_ch, two_ch = bot.get_partial_messageable(id_dict['one'][1]), bot.get_partial_messageable(id_dict['two'][1])
+    one_ch, two_ch = bot.get_partial_messageable(cfg.id_dict['one'][1]), bot.get_partial_messageable(cfg.id_dict['two'][1])
     count_manage(-num, False)
     if count < 0:
         count_manage(num, False)
@@ -182,40 +169,40 @@ async def out(
         await ctx.respond(embed=embed)
         return
 
-    if str(ctx.channel.id) == id_dict['one'][1]:
-        embed = add_embed("利用通知", f'{first_server_name}で{num}人退室しました。現在の利用人数は{count}人です。', "one")
+    if str(ctx.channel.id) == cfg.id_dict['one'][1]:
+        embed = add_embed("利用通知", f'{cfg.first_server_name}で{num}人退室しました。現在の利用人数は{count}人です。', "one")
         await ctx.respond(embed=embed)
         await two_ch.send(embed=embed)
-        write_logfile(ctx.author, num, "out", first_server_name, count)
+        write_logfile(ctx.author, num, "out", cfg.first_server_name, count)
 
-    elif str(ctx.channel.id) == id_dict['two'][1]:
-        embed = add_embed("利用通知", f'{second_server_name}で{num}人退室しました。現在の利用人数は{count}人です。', "two")
+    elif str(ctx.channel.id) == cfg.id_dict['two'][1]:
+        embed = add_embed("利用通知", f'{cfg.second_server_name}で{num}人退室しました。現在の利用人数は{count}人です。', "two")
         await ctx.respond(embed=embed)
         await one_ch.send(embed=embed)
-        write_logfile(ctx.author, num, "out", second_server_name, count)
+        write_logfile(ctx.author, num, "out", cfg.second_server_name, count)
 
-@bot.slash_command(guild_ids=[id_dict['one'][0], id_dict['two'][0]], description="現在の人数が部屋の人数と会わない場合、気づいた人が現在人数を設定しなおしてください。")
+@bot.slash_command(guild_ids=[cfg.id_dict['one'][0], cfg.id_dict['two'][0]], description="現在の人数が部屋の人数と会わない場合、気づいた人が現在人数を設定しなおしてください。")
 async def set(
     ctx,
     num: Option(int, '現在の人数を入力してください'),
 ):
     count_manage(num, True)
-    one_ch, two_ch = bot.get_partial_messageable(id_dict['one'][1]), bot.get_partial_messageable(id_dict['two'][1])
-    if count < 0 or count > max_count:
-        embed = add_embed("エラー", "0人未満または" + str(max_count) + "人以下の数字を入力してください。", "er")
+    one_ch, two_ch = bot.get_partial_messageable(cfg.id_dict['one'][1]), bot.get_partial_messageable(cfg.id_dict['two'][1])
+    if count < 0 or count > cfg.max_count:
+        embed = add_embed("エラー", "0人未満または" + str(cfg.max_count) + "人以下の数字を入力してください。", "er")
         await ctx.respond(embed=embed)
         return
     
-    if str(ctx.channel.id) == id_dict['one'][1]:
-        embed = add_embed("現在の人数", f'現在の人数は{count}人です。\n({first_server_name}で編集されました。)', "one")
+    if str(ctx.channel.id) == cfg.id_dict['one'][1]:
+        embed = add_embed("現在の人数", f'現在の人数は{count}人です。\n({cfg.first_server_name}で編集されました。)', "one")
         await ctx.respond(embed=embed)
         await two_ch.send(embed=embed)
-        write_logfile(ctx.author, num, "set", first_server_name, count)
+        write_logfile(ctx.author, num, "set", cfg.first_server_name, count)
     else:
-        embed = add_embed("現在の人数", f'現在の人数は{count}人です。\n({second_server_name}で編集されました。)', "two")
+        embed = add_embed("現在の人数", f'現在の人数は{count}人です。\n({cfg.second_server_name}で編集されました。)', "two")
         await ctx.respond(embed=embed)
         await one_ch.send(embed=embed)
-        write_logfile(ctx.author, num, "set", second_server_name, count)
+        write_logfile(ctx.author, num, "set", cfg.second_server_name, count)
 
 @bot.slash_command(description="使わないでください。botを停止します。") #botをログファイルを閉じて停止させる
 async def stop(ctx):
@@ -223,11 +210,11 @@ async def stop(ctx):
     await ctx.respond("botを停止しました。")
     await bot.close()
 
-@tasks.loop(seconds=60)
-async def loop():
-    now = datetime.now().strftime('%H:%M')
-    if now == '07:00':
-        channel = bot.get_channel(CHANNEL_ID)
-        await channel.send('おはよう')  
+#@tasks.loop(seconds=30)
+#async def loop(self):
+#    now = datetime.now().strftime('%H:%M')
+#    print("act! " + now)
+#    if now == '17:08':
+#        print("時間だよ")
 
-bot.run(TOKEN)
+bot.run(cfg.TOKEN)
